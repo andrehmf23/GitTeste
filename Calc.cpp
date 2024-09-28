2d05b2df8dd1c846c7dcc1c859fb85e26b511654
andrehmf23 <marchettofilho0404@gmail.com>
2024-09-27 22:17:17 -0300

    READ atualizado!

--- Calc.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-
-int main(){
-    std::cout << "Hello World!";
-    std::cout << "Let's go!";
-    return 0;
-};
---
+++ README.md
@@ -0,0 +1,2 @@
+Engenharia de Software
+Essa tarefa está detinada a ES, ao qual serve para teste no git!
